b2df18b0d09130395d9132db21a2f4faa396e646
ryyanmapes <ryyanmapes@gmail.com>
2021-01-05 17:51:07 -0700

    more tweaks

--- src/main/java/com/example/examplemod/entities/WoodenPushcartEntity.java
+++ src/main/java/com/example/examplemod/entities/WoodenPushcartEntity.java
@@ -33,8 +33,8 @@ public class WoodenPushcartEntity extends AbstractPushcart {
         }
     }
 
-    public double getControlSpeed() { return 10; }
-    public double getUphillSpeed() { return 4.25; }
+    public double getControlSpeed() { return 15; }
+    public double getUphillSpeed() { return 4.2; }
     public double getBrakeSpeed() { return 0.25; }
 
     // Wooden cart speed is capped at half that of a normal cart
