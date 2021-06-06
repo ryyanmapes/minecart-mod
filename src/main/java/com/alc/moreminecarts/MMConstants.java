012263444b5d3649910ef62b4180f7cb5bdd3aa5
ryyanmapes <ryyanmapes@gmail.com>
2021-06-06 15:20:45 -0600

    Tuning changes

--- src/main/java/com/alc/moreminecarts/MMConstants.java
+++ src/main/java/com/alc/moreminecarts/MMConstants.java
@@ -11,8 +11,8 @@ public class MMConstants {
 
     public static final double POWERED_LOCKING_RAIL_SPEED = 0.05;
 
-    public static final float HS_SLOWDOWN = 0.9999f;
-    public static final float HS_AIR_DRAG = 0.9999f;
+    public static final float HS_SLOWDOWN = 0.995f;
+    public static final float HS_AIR_DRAG = 0.995f;
 
     public static final float PISTON_PUSHCART_VERTICAL_SPEED = 0.2f;
     public static final float PISTON_PUSHCART_AERODYNAMIC_VERTICAL_SPEED = 0.4f;
