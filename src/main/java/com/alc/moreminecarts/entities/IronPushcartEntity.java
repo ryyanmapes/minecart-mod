399d9999a6955548fa0372691e53205c976ff8f7
ryyanmapes <ryyanmapes@gmail.com>
2022-04-03 15:22:33 -0600

    Small import fix

--- src/main/java/com/alc/moreminecarts/entities/IronPushcartEntity.java
+++ src/main/java/com/alc/moreminecarts/entities/IronPushcartEntity.java
@@ -1,11 +1,23 @@
 package com.alc.moreminecarts.entities;
 
 import com.alc.moreminecarts.MMItemReferences;
+import com.mojang.datafixers.util.Pair;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Vec3i;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseRailBlock;
+import net.minecraft.world.level.block.PoweredRailBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.RailShape;
+import net.minecraft.world.phys.Vec3;
 
 public class IronPushcartEntity extends AbstractPushcart {
 
