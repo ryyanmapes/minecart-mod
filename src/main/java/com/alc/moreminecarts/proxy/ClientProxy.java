dd9c7a81b25b9278abf550108920212fe4bbd0cc
zach2039 <sumyunguy2007@gmail.com>
2022-07-23 05:22:45 -0500

    Prevent keybind event from firing on server

--- src/main/java/com/alc/moreminecarts/proxy/ClientProxy.java
+++ src/main/java/com/alc/moreminecarts/proxy/ClientProxy.java
@@ -7,11 +7,12 @@ import com.alc.moreminecarts.client.PistonPushcartUpKey;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid =  MMConstants.modid, bus = Mod.EventBusSubscriber.Bus.FORGE)
+@Mod.EventBusSubscriber(modid =  MMConstants.modid, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ClientProxy implements IProxy {
 
     @Override
