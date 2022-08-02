28a10be4a877c4285c2c29e09887b6cf90a6604c
zach2039 <sumyunguy2007@gmail.com>
2022-08-02 13:45:11 -0500

    Remove unused event stuff from ClientProxy

--- src/main/java/com/alc/moreminecarts/proxy/ClientProxy.java
+++ src/main/java/com/alc/moreminecarts/proxy/ClientProxy.java
@@ -1,18 +1,9 @@
 package com.alc.moreminecarts.proxy;
 
-import com.alc.moreminecarts.MMConstants;
-import com.alc.moreminecarts.MoreMinecartsMod;
-import com.alc.moreminecarts.client.PistonPushcartDownKey;
-import com.alc.moreminecarts.client.PistonPushcartUpKey;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid =  MMConstants.modid, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ClientProxy implements IProxy {
 
     @Override
